dfcf39ddb4217c135bdc7bf83372e28450b7781b
zengningzhong <zengningzhong@bitmart.com>
2020-06-01 16:14:00 +0800

    use GlobalConst.CLOUD_URL for test

--- src/test/java/com/bitmart/api/contract/prv/TestSpot.java
+++ src/test/java/com/bitmart/api/contract/prv/TestSpot.java
@@ -3,6 +3,7 @@ package com.bitmart.api.contract.prv;
 import com.bitmart.api.Call;
 import com.bitmart.api.CloudContext;
 import com.bitmart.api.common.CloudException;
+import com.bitmart.api.common.GlobalConst;
 import com.bitmart.api.key.CloudKey;
 import com.bitmart.api.request.spot.prv.*;
 import com.bitmart.api.request.spot.pub.*;
@@ -12,8 +13,8 @@ import org.junit.jupiter.api.Test;
 
 public class TestSpot {
 
-     private static String CLOUD_URL =  "http://api-cloud.bitmartdev.com";;
-//    private static String CLOUD_URL = GlobalConst.CLOUD_URL;
+//     private static String CLOUD_URL =  "http://api-cloud.bitmartdev.com";;
+    private static String CLOUD_URL = GlobalConst.CLOUD_URL;
     private static String API_KEY = "80618e45710812162b04892c7ee5ead4a3cc3e56";
     private static String API_SECRET = "6c6c98544461bbe71db2bca4c6d7fd0021e0ba9efc215f9c6ad41852df9d9df9";
     private static String API_MEMO = "test001";
